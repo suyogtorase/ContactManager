ae659ab5fb5a5a5152922c90bbd760ee359bfc53
suyogtorase <sst124435@gmail.com>
2026-01-03 14:38:19 +0530

    For deployment

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.use(express.json());
 
 mongoose
